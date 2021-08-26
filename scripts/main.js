f58af7d8baf727e8f4e19fb4de861be72642e835
jirrasrivani <66119861+jirrasrivani@users.noreply.github.com>
2021-08-26 22:31:18 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,6 @@
-
-
-/*
-Variable naming convention: <object>_<action>_<objectname>; Example -> Button_click_b1;
-*/
-
-//Variables (BE CAREFUL THESE MIGHT BE USED IN OTHER JS FILES TOO)
 var inp_as=document.getElementById('a_size'),array_size=inp_as.value;
 var inp_gen=document.getElementById("a_generate");
 var inp_aspeed=document.getElementById("a_speed");
-//var array_speed=document.getElementById('a_speed').value;
 
 var butts_algos=document.querySelectorAll(".algos button");
 
